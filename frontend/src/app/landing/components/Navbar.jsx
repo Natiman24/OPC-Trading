2b9ea8603c0fccafb927436ca84648d6a9f7d6af
abrsh-10 <abrhamsisay33@gmail.com>
2024-05-17 23:49:40 +0300

    navbar logo padding top added

--- frontend/src/app/landing/components/Navbar.jsx
+++ frontend/src/app/landing/components/Navbar.jsx
@@ -4,8 +4,10 @@ export default function Navbar() {
 
   return (
     <>
-     <nav className="h-24 w-full bg-background flex justify-between px-10 mt-0 items-center">
-        <img className="text-white" src="./logo.png" alt="logo" />
+     <nav className="h-28 w-full bg-background flex justify-between px-10 py-10 items-center">
+      <div className="my-10">
+        <img src="./logo.png" alt="logo" />
+      </div>
         <ul className="flex gap-10 text-white font-lato text-[1.2rem] mr-20">
           <li className="hover:cursor-pointer hover:border-b-2">Home</li>
           <li className="hover:cursor-pointer hover:border-b-2">Services</li>
