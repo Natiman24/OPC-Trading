25a36ef4f746e215cfdb3b397ec0dcda52ac673e
abrsh-10 <abrhamsisay33@gmail.com>
2024-05-19 11:27:25 +0300

    carousel added

--- frontend/src/app/landing/components/carousel.jsx
+++ frontend/src/app/landing/components/carousel.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+'use client'
+
+import {React , useState} from 'react'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const Carousel = () => {
+
+  const logos = [
+    "./carousel-1.png",
+    "./carousel-2.png",
+    "./carousel-3.png",
+    "./carousel-4.png",
+  ];
+
+  const [settings] = useState({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 3000,
+    cssEase: 'linear',
+    arrows:false
+  });
+
   return (
-    <div className='text-center text-5xl text-white bg-background h-96 border-none -mt-0.5 py-8'>Carousel</div>
+    <div className='bg-background w-full'>
+     <div className=' py-20 -mt-0.5 max-w-screen-2xl mx-auto'>
+       <Slider {...settings}>
+        {logos.map((logo , index) => {
+          return (
+           <div key={index} className='grid place-items-center'>
+            <img src={logo} alt='logo' className='w-40 mx-auto'/>
+           </div>
+          )
+        })}
+       </Slider>
+    </div>
+    </div>
+
   )
 }
 
