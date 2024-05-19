a37e617ea4f03c16bc9b1cd4c593cd1d776afce7
abrsh-10 <abrhamsisay33@gmail.com>
2024-05-19 22:18:39 +0300

    services responsivess on large screen

--- frontend/src/app/landing/components/Services.jsx
+++ frontend/src/app/landing/components/Services.jsx
@@ -5,9 +5,9 @@ const Service = () => {
     <div className='text-white md:py-20 grid md:grid-cols-2 grid-cols-1 gap-12 items-start justify-evenly bg-background max-md:mt-16'>
         <div className='flex flex-col gap-8'>
             <h3 className='text-[3.5rem] md:text-[4rem] lg:text-[4.5rem] xl:text-[5rem] font-fugaz text-white md:pl-14 lg:pl-20 xl:pl-28 max-md:text-center leading-none'>Services</h3>
-            <img src="./service.png" alt="services" className=' max-md:w-[80%] max-md:mx-[10%] ml-5 md:w-full xl:ml-16 max-md:hidden' />
+            <img src="./service.png" alt="services" className=' max-md:w-[80%] max-md:mx-[10%] ml-5 md:w-2/3 md:mx-auto xl:ml-16 max-md:hidden' />
         </div>
-        <div className='flex flex-col gap-7 items-start px-5 2xl:pl-30'>
+        <div className='flex flex-col gap-7 items-start px-5 2xl:pl-40'>
            <div className='w-52 h-2 bg-white max-md:hidden'></div>
            <div className=' text-center md:text-left text-white flex flex-col gap-4'>
             <h5 className='text-3xl font-bold font-lato'>Sales & After Sales</h5>
