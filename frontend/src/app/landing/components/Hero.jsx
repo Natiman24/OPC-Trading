097c21c5ba80951effac240daf1f2c30d76e0ab3
abrsh-10 <abrhamsisay33@gmail.com>
2024-05-19 22:05:30 +0300

    services section is responsive

--- frontend/src/app/landing/components/Hero.jsx
+++ frontend/src/app/landing/components/Hero.jsx
@@ -5,21 +5,23 @@ import { SectionWrapper } from '../hoc';
 
 const Hero = () => {
   return (
-    <div className='w-full mb-10 flex max-md:grid-cols-1 max-md:flex-col-reverse'>
+    <div className='flex flex-col mb-10'>
+    <div className='w-full flex max-md:grid-cols-1 max-md:flex-col-reverse'>
         <div className='flex flex-col gap-y-10'>
           <h1 className='font-fugaz text-[3rem] md:text-[4rem] lg:text-[7rem] text-white md:text-left text-center pt-8 lg:pt-28 sm:pl-6 md:pl-10 leading-tight'>OPC <br className='max-md:hidden'/> <span className='md:ml-10'>Trading</span></h1>
           <p className='font-lato text-2xl md:text-left text-center text-white md:max-w-xl lg:max-w-max pl-4 md:pl-10 max-md:pr-4'>
             Unleash unmatched printing prowess with our sales, maintenance, repair services, and cutting-edge equipment. Elevate your business!
           </p>
-          <button className='bg-white text-black md:self-start self-center max-md:mb-10 w-32 h-14 sm:w-32 sm:h-12 sm:text-2xl lg:w-40 lg:h-16 rounded-2xl lg:text-2xl font-lato md:ml-10'>
+          <button className='bg-white text-black md:self-start self-center max-md:mb-10 w-40 h-16 rounded-2xl text-2xl font-lato md:ml-10'>
             Explore
           </button>
         </div>
         <div className='flex items-start justify-center gap-x-10'>
             <img src="./hero-image-1.png" alt="hero-image" className='max-md:hidden w-[40%]'/>
             <img src="./hero-image-2.png" alt="hero-image" className='max-md:hidden w-[40%]'  />
-
         </div>
+    </div>
+      <img src="./hero-mobile.png" alt="hero-image" className='md:hidden w-[80%] mx-auto h-52'  />
     </div>
   )
 }
--- frontend/src/app/landing/components/Services.jsx
+++ frontend/src/app/landing/components/Services.jsx
@@ -2,37 +2,40 @@ import React from 'react'
 
 const Service = () => {
   return (
-    <div className='text-white py-20 flex flex-wrap gap-20 items-start justify-evenly lg:gap-48 bg-background'>
+    <div className='text-white md:py-20 grid md:grid-cols-2 grid-cols-1 gap-12 items-start justify-evenly bg-background max-md:mt-16'>
         <div className='flex flex-col gap-8'>
-            <h3 className='text-[5rem] font-fugaz text-white pl-28 leading-none'>Services</h3>
-            <img src="./service.png" alt="services" className=' w-full ml-16' />
+            <h3 className='text-[3.5rem] md:text-[4rem] lg:text-[4.5rem] xl:text-[5rem] font-fugaz text-white md:pl-14 lg:pl-20 xl:pl-28 max-md:text-center leading-none'>Services</h3>
+            <img src="./service.png" alt="services" className=' max-md:w-[80%] max-md:mx-[10%] ml-5 md:w-full xl:ml-16 max-md:hidden' />
         </div>
-        <div className='flex flex-col gap-7 items-start pl-5 lg:pl-40'>
-           <div className='w-52 h-2 bg-white'></div>
-           <div className='text-left text-white flex flex-col gap-4'>
+        <div className='flex flex-col gap-7 items-start px-5 2xl:pl-30'>
+           <div className='w-52 h-2 bg-white max-md:hidden'></div>
+           <div className=' text-center md:text-left text-white flex flex-col gap-4'>
             <h5 className='text-3xl font-bold font-lato'>Sales & After Sales</h5>
-            <p className='font-lato text-lg'>
+            <p className='font-lato text-lg '>
                 Lorem ipsum dolor sit amet,
                 consectetur adipiscing elit. Faucibus in libero risus
             </p>
            </div>
-           <div className='text-left text-white flex flex-col gap-4'>
+           <div className=' text-center md:text-left text-white flex flex-col gap-4'>
             <h5 className='text-3xl font-bold font-lato'>Maintenance</h5>
-            <p className='font-lato text-lg'>
+            <p className='font-lato text-lg max-w-[660px] mx-auto'>
                 Lorem ipsum dolor sit amet,
                 consectetur adipiscing elit. Faucibus in libero risus
             </p>
            </div>
-           <div className='text-left text-white flex flex-col gap-4'>
+           <div className=' text-center md:text-left text-white flex flex-col gap-4'>
             <h5 className='text-3xl font-bold font-lato'>Repairment</h5>
-            <p className='font-lato text-lg'>
+            <p className='font-lato text-lg '>
                 Lorem ipsum dolor sit amet,
                 consectetur adipiscing elit. Faucibus in libero risus
             </p>
            </div>
-           <button className='bg-white text-black self-start w-36 h-14 rounded-2xl text-2xl font-lato'>
+           <button className='bg-white text-black max-md:my-5 self-center md:self-start w-36 h-14 rounded-2xl text-2xl font-lato'>
             Explore
           </button>
+
+          <img src="./service.png" alt="services" className=' w-[80%] mx-[10%] md:hidden'/>
+
         </div>
 
     </div>
